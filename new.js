e299ede19859e86544b42aacb9368d468b069830
puja <proy87830@gmail.com>
2025-07-15 22:48:55 +0530

    all update

---
+++ new.js
@@ -0,0 +1,62 @@
+const input = document.querySelector("#input");
+const btnEle = document.querySelector("#btn");
+const output = document.querySelector(".out");
+
+const array = [];
+
+function check(tasks, arr) {
+  if (tasks === "") {
+    input.value = "";
+    return false;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (tasks === arr[i].name) {
+      alert("same task");
+      return false;
+    }
+  }
+  return true;
+}
+
+function render(array) {
+  output.innerHTML = "";
+  for (let i = 0; i < array.length; i++) {
+    const divEle = document.createElement("div");
+    output.appendChild(divEle);
+    divEle.className = "innerDiv";
+    const span = document.createElement("span");
+    span.textContent = "\u00d7";
+    span.className = "close";
+    divEle.appendChild(span);
+    divEle.innerHTML += `${array[i].name}`;
+  }
+}
+
+// function select() {
+//   const span = document.createElement("span");
+//   span.textContent = "\u00d7";
+//   span.className = "close";
+//   divEle.appendChild(span);
+// }
+
+function add() {
+  const tasks = input.value.trim();
+  if (check(tasks, array)) {
+    array.push({
+      name: tasks,
+      completed: false,
+    });
+  }
+  render(array);
+  input.value = "";
+}
+
+btnEle.addEventListener("click", add);
+
+input.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") add();
+});
+
+span.addEventListener("click", () => {
+  divEle.remove();
+})
